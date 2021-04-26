c5d04b3dcb0c1ccc61daf1926c8d6566b304daa2
P0shJosh <jphodge.96@gmail.com>
2021-04-26 14:36:34 +0100

    FEAT: Include JS and matching views for user sorting products

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 # Create your views here.
@@ -20,12 +21,14 @@ def all_products(request):
             if sortkey == 'name'
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category_name'  
                 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
-                products = products.order_by(sortkey)
+            products = products.order_by(sortkey)
         
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
